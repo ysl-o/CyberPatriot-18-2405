654b61db90fc97fbb0e70bf2a1d91cb96fd73fa5
yslo <158855392+ysl-o@users.noreply.github.com>
2025-10-23 16:14:00 -0500

    Update cplinuxsecurity.sh

--- Linux/cplinuxsecurity.sh
+++ Linux/cplinuxsecurity.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 # DO NOT USE - NOT FINISHED AND DEBUGGED
 
+echo "Updating packages..."
+sudo apt update -y && sudo apt full-upgrade -y && sudo apt-get update -y && sudo apt-get dist-upgrade -y
+echo ""
+echo "----"
+echo ""
+echo "Installing SSH and libpam-cracklib for file locations..."
+sudo apt install openssh-server -y
+sudo apt-get install libpam-cracklib -y
+
 mapfile -t NECESSARY_PROGRAMS < "$1"
 RELEVANT_LINE=0
 
 SYSCTL_CONFIG="/etc/sysctl.conf"
 PASS_POLICY_FILE="/etc/login.defs"
+
+sudo 
 SSH_PERM_FILE="/etc/ssh/sshd_config"
 
 sudo apt-get install libpam-cracklib -y
@@ -22,18 +33,18 @@ source ~/.bashrc
 ROOT_PASSWORD=$(tr -dc 'A-Za-z0-9!@#$%^&*()' < /dev/urandom | head -c 15)
 sudo usermod --password "$ROOT_PASSWORD" root
 sudo passwd -l root
+echo ""
+echo "----"
+echo ""
 echo "Changed root password to ${ROOT_PASSWORD}"
 echo "Disabled logging in to root account"
-echo ""
 
-sudo apt update -y && sudo apt full-upgrade -y && sudo apt-get update -y && sudo apt-get dist-upgrade -y
 sudo apt install ufw -y
 sudo ufw disable
 sudo ufw enable
 
 sudo sed -i -e 's/.*net.ipv4.tcp_syncookies.*/net.ipv4.tcp_syncookies=1/' -e 's/.*net.ipv4.conf.default.rp_filter.*/net.ipv4.conf.default.rp_filter=1/' -e 's/.*net.ipv4.conf.all.rp_filter.*/net.ipv4.conf.all.rp_filter=1/' -e 's/.*net.ipv4.conf.all.secure_redirects.*/net.ipv4.conf.all.secure_redirects=1/' -e 's/.*net.ipv6.conf.all.accept_redirects.*/net.ipv6.conf.all.accept_redirects=0/' -e 's/.*net.ipv4.conf.all.send_redirects.*/net.ipv4.conf.all.send_redirects=0/' -e 's/.*net.ipv4.conf.all.accept_source_route.*/net.ipv4.conf.all.accept_source_route=0/' -e 's/.*net.ipv6.conf.all.accept_source_route.*/net.ipv6.conf.all.accept_source_route=0/' -e 's/.*net.ipv4.conf.all.log_martians.*/net.ipv4.conf.all.log_martians=1/' -e 's/.*kernel.sysrq.*/kernel.sysrq=0/' "$SYSCTL_CONFIG"
 echo "Enabled TCP SYN cookie protection to prevent denial of service (DOS)"
-echo ""
 
 RELEVANT_LINE=$(grep -n "PASS_MAX_DAYS" "$PASS_POLICY_FILE" | awk -F: 'NR==2 {print $1}')
 sudo sed -i \
@@ -42,18 +53,15 @@ sudo sed -i \
 -e "$((RELEVANT_LINE + 2))c\\PASS_WARN_AGE 10" \
 "$PASS_POLICY_FILE"
 echo "Modified password time policy"
-echo ""
 
 sudo touch "$SSH_PERM_FILE"
 grep -n -m 1 "PermitRootLogin" "$SSH_PERM_FILE" | awk -F: '{$RELEVANT_LINE=$1}'
 sudo sed -i -e "${RELEVANT_LINE}c\\PermitRootLogin no" "$SSH_PERM_FILE"
 echo "Removed ability to login to SSH using the root"
-echo ""
 
 grep -n -m 1 "autologin-user" "$AUTO_LOGIN" | awk -F: '{$RELEVANT_LINE=$1}'
 sudo sed -i -e "${RELEVANT_LINE}d" "$AUTO_LOGIN"
 echo "Removed having an automatic login user; not the user itself"
-echo ""
 
 grep -n -m 1 "allow_guest" "$AUTO_LOGIN" | awk -F: '{$RELEVANT_LINE=$1}'
 sudo sed -i \
@@ -61,24 +69,22 @@ sudo sed -i \
 -e '$a\allow_guest=false\' \
 "$AUTO_LOGIN"
 echo "Does not allow a guest account to the computer"
-echo ""
 
 sudo touch "$PERIODIC"
 sudo sed -i '$a\APT::Periodic::Update-Package-Lists "1"\' "$PERIODIC"
 echo "Set automatic package updating"
-echo ""
 
 grep -n -m 1 "pam_unix.so" "$PAM_COMMON_PASS" | awk -F: '{$RELEVANT_LINE=$1}'
 sed -i -e "${RELEVANT_LINE}s,$, remember=5," "$PAM_COMMON_PASS"
 echo "Set to remember last 10 user passwords"
-echo ""
 
 grep -n -m 1 "pam_cracklib.so" "$PAM_COMMON_PASS" | awk -F: '{$RELEVANT_LINE=$1}'
 sudo sed -i -e "${RELEVANT_LINE}s,$, minlen=14 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1," "$PAM_COMMON_PASS"
 echo "Set minimum password policies with all security requirements"
-echo ""
 
+echo ""
 echo "----"
+echo ""
 echo "Removing extraneous media files..."
 MEDIA_FILES=("jpg" "png" "gif" "webp" "mp4" "mov" "avi" "webm" "mp3" "wav" "aac" "flac" "ogg")
 for FILE_EXTENSION in "${MEDIA_FILES[@]}"; do
@@ -92,7 +98,9 @@ for FILE_EXTENSION in "${MEDIA_FILES[@]}"; do
     done
 done
 
+echo ""
 echo "----"
+echo ""
 echo "Removing hacking tools and clients..."
 HACKS=("dnsrecon" "dnsenum" "proxychains" "tor" "nmap" "slowloris" "zphisher" "nikto" "openvas" "metasploit" "sqlmap" "searchsploit" "hydra" "john" "john-the-ripper" "hashcat" "aircrack-ng" "wifite" "burp" "owasp" "dirb" "gobuster" "empire", "mimikatz" "netcat" "ncat" "lynis" "wireshark")
 for HACK in "${HACKS[@]}"; do
@@ -106,7 +114,9 @@ for HACK in "${HACKS[@]}"; do
     fi
 done
 
+echo ""
 echo "----"
+echo ""
 echo "Updating systemd..."
 sudo apt-get install build-essential devscripts
 sudo apt-get build-dep systemd
@@ -124,13 +134,17 @@ cd ../systemd-220
 dpkg-buildpackage -us -uc
 cd ..
 sudo dpkg -i *.deb
-echo "----"
 
 # Run last; may not work
+echo ""
+echo "----"
+echo ""
 echo "Running Clam antivirus..."
 sudo apt-get install clamav
 sudo freshclam
 sudo clamscan -i -r --remove=yes /
 
+echo ""
 echo "----"
+echo ""
 echo "It is highly recommended that you reboot at this point."
