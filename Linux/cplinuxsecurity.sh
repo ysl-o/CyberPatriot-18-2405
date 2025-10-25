c06b99afa42c20ba188004d87c8b572436552d5a
yslo <158855392+ysl-o@users.noreply.github.com>
2025-10-25 16:35:34 -0500

    Update cplinuxsecurity.sh

--- Linux/cplinuxsecurity.sh
+++ Linux/cplinuxsecurity.sh
@@ -61,12 +61,16 @@ while IFS=: read -r username _ uid _ _ _ _; do
 done < /etc/passwd
 
 for USERNAME in "${reg_users[@]}"; do
-	if [[ "$USERNAME" == "$USER" ]]; then continue fi
+	if [[ "$USERNAME" == "$USER" ]]; then
+		continue
+	fi
 	found=0
 	for authorized in "${all_users[@]}"; do
-		if [[ authorized == USERNAME ]]; then found=1 fi
+		if [[ "$authorized" == "$USERNAME" ]]; then
+			found=1
+		fi
 	done
-	if (( found == 0 )); then
+	if [ "$found" -eq 0 ]; then
 		sudo deluser --remove-home "$USERNAME"
         echo " - Deleted user ${USERNAME}"
 	fi
